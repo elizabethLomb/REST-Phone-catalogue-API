d882eef81a446155f08c1ac2aa50c8bd8b43524e
elizabethLomb <elizabeth.lombardi7@gmail.com>
2020-06-01 17:28:18 +0200

    init commit rest API working and implemented width HEROKU

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-//mongodb://localhost:27017/phones
-const MONGODB_URI = process.env.MONGODB_URI || '';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/phones';
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => console.info(`Successfully connected to the database ${MONGODB_URI}`))
